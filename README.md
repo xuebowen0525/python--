e565fd0432876a331f878ec321cd786d2032c028
unknown <xbw0525@126.com>
2018-12-10 11:50:15 +0800

    修复desc内容会报错的问题

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python
 python爬虫
+爬取豆瓣读书-小说下的书籍保存到excle
--- 豆瓣读书.py
+++ 豆瓣读书.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import xlsxwriter
-import os
+import os,stat
 def writeExcel(row=0):  
        if row ==0:
            worksheet.write(row,0,'书名')
@@ -13,6 +13,7 @@ def writeExcel(row=0):
            worksheet.write(row,1,list[row-1][1])
            worksheet.write(row,2,list[row-1][2])
            worksheet.write(row,3,list[row-1][3])
+           
        
 def get_info(url):
     html = requests.get(url).text
@@ -20,36 +21,39 @@ def get_info(url):
     data = Soup.find('ul',{'class':'subject-list'}).find_all('li')
     for info in data:
         #标题
-        titles = info.find('div',{'class':'info'})
-        title = str(titles.find('a').text).replace(' ', '')
+        titles = info.find('div',{'class':'info'}).find('a')
+        title = titles.get_text(strip=True).replace(' ','')
         #作者
         authors = info.find('div',{'class':'pub'})
-        author = authors.text.replace(' ','').split('/')[0]
+        author = authors.get_text(strip=True).split('/')[0].replace(' ','')
         #评价
-        rating_nums = info.find('div',{'class':'star clearfix'})
-        rating_num = rating_nums.find('span',{'class':'rating_nums'}).text
+        rating_nums = info.find('span',{'class':'rating_nums'})
+        rating_num = rating_nums.get_text(strip=True)
         #描述
-        descs = info.find('p')
-        desc = descs.text.replace('\u2022','').replace('\u2027','').replace('\u22ef','')
+        descs = info.find('div',{'class':'info'}).find_all('p')
+        desc = descs[0].get_text()
         list.append([title,author,rating_num,desc])
     return list
    
 
 if __name__ =='__main__':
-    workbook = xlsxwriter.Workbook("C:\\Users\\Administrator\\Desktop\\python\\豆瓣读书.xlsx")
+    workbook = xlsxwriter.Workbook("豆瓣读书.xlsx")
     worksheet = workbook.add_worksheet('小说')
     list=[]
     page =0
+    #url中start参数的值
     i = 0
+    #要爬取的页数
+    y = 1
     writeExcel(row=0)
-    while page <= 20:
+    while i <=(y-1)*20:
         url = 'https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4?start='+ str(i) + '&type=T'
         get_info(url)
         page +=1
         i = page*20
-    row = 1
+    row = 1 
+    #将getinfo()返回的list中的数据保存到excle中
     while row <= len(list):        
         writeExcel(row=row)
         row = row +1
     workbook.close()
-    
